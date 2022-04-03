4cfa63b799446ce0ff969a1a1765973628d37df2
shayanshafquat <shafquat.shayan@gmail.com>
2022-04-04 00:42:05 +0530

    Adding the kfast channel in NeuroML format

--- NEURON/test/analyse_mods.sh
+++ NEURON/test/analyse_mods.sh
@@ -1,6 +1,6 @@
 #pynml-modchananalysis nat -stepV 5 -temperature [37] -modFile '../channels/nat.mod'
 #pynml-modchananalysis nap -stepV 5 -temperature [37] -modFile '../channels/nap.mod'
-#pynml-modchananalysis kfast -stepV 5 -temperature [37] -modFile '../channels/kfast.mod'
+pynml-modchananalysis kfast -stepV 5 -temperature 37
 #pynml-modchananalysis km -stepV 5 -temperature [37] -modFile '../channels/IKM.mod'
 #pynml-modchananalysis sca -stepV 5 -temperature [37] -modFile '../channels/SlowCa.mod'
 #pynml-modchananalysis h -stepV 5 -temperature 37 -modFile '../channels/h.mod'
--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -3,10 +3,10 @@ from pyneuroml.neuron import export_to_neuroml2
 import sys
 import os
 
-os.chdir("../NEURON/test")
+os.chdir("../NEURON/init_models_with_ca")
 sys.path.append(".")
 
-export_to_neuroml2("load_model2.hoc", 
-                   "../../NeuroML2/L5_model2.cell.nml", 
+export_to_neuroml2("init_model2.hoc", 
+                   "../../NeuroML2/init_model2.cell.nml", 
                    includeBiophysicalProperties=False,
                    known_rev_potentials={"na":60,"k":-90,"ca":140})
