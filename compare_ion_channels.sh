cfedbe28de6836a9aa795957d0dd8d20f45caf1c
shayanshafquat <shafquat.shayan@gmail.com>
2022-07-07 01:43:46 +0530

    adding ih to channel summary page

--- compare_ion_channels.sh
+++ compare_ion_channels.sh
@@ -1,8 +1,10 @@
 echo "Simulating using .mod file"
 cd NEURON/channels
 #pynml-modchananalysis km -stepV 5 -temperature 37
+#pynml-mochananalysis nat -stepV 5 -temperature 37 -dt 0.0025
+#pynml-modchananalysis kslow -stepV 10 -temperature 37 -duration 100000 -modFile './NEURON/channels/kslow.mod'  # Takes ~45mins
 pynml-modchananalysis -temperature 37 -modFile IKM.mod km
 
+echo "Simulating using .nml file"
 cd ../../NeuroML2
 pynml-channelanalysis IKM.channel.nml -step 5 -temperature 37
-echo "Simulating using .nml file"
